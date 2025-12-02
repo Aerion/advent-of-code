aa68c566f7e8d1e1169a0b10bcdb821120d49428
Guillaume Taquet Gasperini <perso@aerion.me>
2025-12-02 06:06:31 +0100

    2025: add day2 - part1

---
+++ 2025/day2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from aocd import puzzle, submit
+from collections import defaultdict, deque
+from dataclasses import dataclass, field
+from datetime import timedelta
+from functools import cache
+from rich import print
+from sys import stderr
+from typing import Optional
+import time
+
+EXAMPLE_IDX = None
+_start_time = time.time()
+
+data = (puzzle.examples[EXAMPLE_IDX] if EXAMPLE_IDX is not None else puzzle).input_data
+if EXAMPLE_IDX == 0:
+    # Override data if needed
+    # data = """REPLACE_ME"""
+    pass
+
+print(f"Puzzle #{puzzle.day}", file=stderr)
+
+if EXAMPLE_IDX is not None:
+    print(f"Using example #{EXAMPLE_IDX} data", file=stderr)
+else:
+    print(f"Using PROD data", file=stderr)
+
+#################################################################
+# No changes before this line
+#################################################################
+
+result = 0
+for line in data.split(","):
+    first_id, last_id = [int(x) for x in line.split("-")]
+    for i in range(first_id, last_id + 1):
+        s = str(i)
+        if len(s) % 2 == 0 and s[:len(s) // 2] == s[len(s) // 2:]:
+            result += i
+
+#################################################################
+# No changes after this line
+#################################################################
+
+_end_time = time.time()
+print(f"Result: {result} (in {timedelta(seconds=_end_time - _start_time)})", file=stderr)
+if result is not None and EXAMPLE_IDX is None and data == puzzle.input_data:
+    submit(result)
