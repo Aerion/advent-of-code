a8c2776ced9ff13c591134a16ba2868f28dcb929
Guillaume Taquet Gasperini <perso@aerion.me>
2025-12-04 06:16:47 +0100

    2025: add day4 - part1

---
+++ 2025/day4.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+from aocd import puzzle, submit
+from collections import defaultdict, deque
+from dataclasses import dataclass, field
+from datetime import timedelta
+from functools import cache
+from rich import print
+from sys import stderr
+from typing import Optional
+import time
+
+EXAMPLE_IDX = None
+_start_time = time.time()
+
+data = (puzzle.examples[EXAMPLE_IDX] if EXAMPLE_IDX is not None else puzzle).input_data
+if EXAMPLE_IDX == 0:
+    # Override data if needed
+    # data = """REPLACE_ME"""
+    pass
+
+print(f"Puzzle #{puzzle.day}", file=stderr)
+
+if EXAMPLE_IDX is not None:
+    print(f"Using example #{EXAMPLE_IDX} data", file=stderr)
+else:
+    print(f"Using PROD data", file=stderr)
+
+#################################################################
+# No changes before this line
+#################################################################
+
+
+result = 0
+grid = data.splitlines()
+
+directions = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1]
+]
+
+R = len(grid)
+C = len(grid[0])
+MAX_ROLLS = 4
+
+def is_roll(r, c):
+    return 0 <= r < R and 0 <= c < C and grid[r][c] == '@'
+    
+
+for r in range(R):
+    for c in range(C):
+        if not is_roll(r, c):
+            print('.', end='')
+            continue
+
+        rolls_count = 0
+        for direction in directions:
+            new_r = direction[0] + r
+            new_c = direction[1] + c
+            if is_roll(new_r, new_c):
+                rolls_count += 1
+            if rolls_count > MAX_ROLLS:
+                break
+        
+        print(rolls_count, end='')
+        if rolls_count < MAX_ROLLS:
+            result += 1
+    print()
+    
+
+
+#################################################################
+# No changes after this line
+#################################################################
+
+_end_time = time.time()
+print(f"Result: {result} (in {timedelta(seconds=_end_time - _start_time)})", file=stderr)
+if result is not None and EXAMPLE_IDX is None and data == puzzle.input_data:
+    submit(result)
