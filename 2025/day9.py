591d1e7dc829a66cd34efbf37e3d34cc025d0f8d
Guillaume Taquet Gasperini <perso@aerion.me>
2025-12-09 08:07:51 +0100

    2025: add day9 - part2

--- 2025/day9.py
+++ 2025/day9.py
@@ -33,31 +33,100 @@ else:
 result = 0
 positions = []
 for line in data.splitlines():
-    r, c = line.split(",")
+    c, r = line.split(",")
     positions.append((int(r), int(c)))
 
 example_values = {
-    (2,5, 9,7): 24,
-    (7,1, 11,7): 35,
-    (7, 3, 2, 3): 6,
-    (2, 5, 7, 11): 50,
+    (3, 7, 1, 11): 15,
+    (7, 9, 5, 9): 3,
+    (5, 9, 3, 2): 24,
 }
 
-max_area = 0
+authorized_points = set()
 for i in range(len(positions)):
+    prev_r, prev_c = positions[i - 1]
+    cur_r, cur_c = positions[i]
+
+    if prev_c == cur_c:
+        # Same columns
+        for r in range(min(prev_r, cur_r), max(prev_r, cur_r) + 1):
+            authorized_points.add((r, cur_c))
+    else:
+        assert prev_r == cur_r
+        # Same rows
+        for c in range(min(prev_c, cur_c), max(prev_c, cur_c) + 1):
+            authorized_points.add((cur_r, c))
+
+min_r = min(positions, key=lambda pos: pos[0])[0] - 1
+max_r = max(positions, key=lambda pos: pos[0])[0] + 1
+min_c = min(positions, key=lambda pos: pos[1])[1] - 1
+max_c = max(positions, key=lambda pos: pos[1])[1] + 1
+
+min_c_per_row = { r: float('infinity') for r in range(min_r, max_r)}
+max_c_per_row = { r: float('-infinity') for r in range(min_r, max_r)}
+min_r_per_column = { r: float('infinity') for r in range(min_c, max_c)}
+max_r_per_column = { r: float('-infinity') for r in range(min_c, max_c)}
+
+for r, c in authorized_points:
+    min_c_per_row[r] = min(min_c_per_row[r], c)
+    max_c_per_row[r] = max(max_c_per_row[r], c)
+    min_r_per_column[c] = min(min_r_per_column[c], r)
+    max_r_per_column[c] = max(max_r_per_column[c], r)
+
+def print_grid():
+    for r in range(min_r, max_r + 1):
+        for c in range(min_c, max_c + 1):
+            if (r, c) in positions:
+                print('O', end='')
+            elif (r, c) in authorized_points:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+
+#print_grid()
+
+import tqdm
+max_area = 0
+for i in tqdm.tqdm(range(len(positions))):
     for j in range(len(positions)):
         if i == j:
             continue
-        x_distance = abs(positions[i][0] - positions[j][0]) + 1
-        y_distance = abs(positions[i][1] - positions[j][1]) + 1
-        area = abs(x_distance * y_distance)
-
-        """
         example_result = example_values.get((*positions[i], *positions[j]))
+        if EXAMPLE_IDX is None:
+            example_result = None
+
+        r_distance = abs(positions[i][0] - positions[j][0]) + 1
+        c_distance = abs(positions[i][1] - positions[j][1]) + 1
+        area = abs(r_distance * c_distance)
+
+        min_r = min(positions[i][0], positions[j][0])
+        max_r = max(positions[i][0], positions[j][0])
+        min_c = min(positions[i][1], positions[j][1])
+        max_c = max(positions[i][1], positions[j][1])
+
+        discarded = False
+        for c in range(min_c, max_c + 1):
+            if min_r < min_r_per_column[c] or max_r > max_r_per_column[c]:
+                discarded = True
+                break
         if example_result:
-            print(x_distance, y_distance, positions[i], positions[j])
+            assert not discarded
+        if discarded:
+            continue
+
+        for r in range(min_r, max_r + 1):
+            if min_c < min_c_per_row[r] or max_c > max_c_per_row[r]:
+                discarded = True
+                break
+        if example_result:
+            assert not discarded
+        if discarded:
+            continue
+
+        if example_result:
+            print(r_distance, c_distance, positions[i], positions[j])
             assert area == example_result
-        """
 
         if area > max_area:
             max_area = area
